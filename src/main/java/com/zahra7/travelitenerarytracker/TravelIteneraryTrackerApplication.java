14b62fce1c49469ba6dc4d16715c1954ae5c6a0a
Fatima Zohra Filali <filalifz7@gmail.com>
2025-08-21 16:43:23 -0400

    Deploying in render (changed deployment to war)

--- src/main/java/com/zahra7/travelitenerarytracker/TravelIteneraryTrackerApplication.java
+++ src/main/java/com/zahra7/travelitenerarytracker/TravelIteneraryTrackerApplication.java
@@ -2,12 +2,18 @@ package com.zahra7.travelitenerarytracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TravelIteneraryTrackerApplication {
+public class TravelIteneraryTrackerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TravelIteneraryTrackerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TravelIteneraryTrackerApplication.class, args);
     }
-
 }
