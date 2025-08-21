9219716c819ac970e47017b2218ba56de3fd52f7
Fatima Zohra Filali <filalifz7@gmail.com>
2025-08-21 10:12:25 -0400

    Initial commit

---
+++ src/main/java/com/zahra7/travelitenerarytracker/TravelIteneraryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.zahra7.travelitenerarytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelIteneraryTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelIteneraryTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zahra7/travelitenerarytracker/TravelIteneraryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zahra7.travelitenerarytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelIteneraryTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
